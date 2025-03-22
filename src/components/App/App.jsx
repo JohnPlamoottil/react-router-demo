b894f19de5f83edd8d091bd25d6dcba29dc40472
Kanna Plamoottil <dev.jjplamoottilaws@gmail.com>
2025-03-22 02:28:35 -0500

    Lesson2.5 part4 dynamic routes Mar12-21,2025

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "../Dashboard/Dashboard";
 import Reviews from "../Reviews/Reviews";
+import Review from "../Review/Review";
 import Header from "../Header/Header";
 import AboutUs from "../AboutUs/AboutUs";
 import AboutMe from "../AboutMe/AboutMe";
@@ -15,6 +17,23 @@ import "./App.css";
 
 function App() {
   // Add the child routes to the /about-me route.
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    // Fetch the review data from the server.
+    fetch("https://api.nomoreparties.co/emoji-critic-ens")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        // Pass the parsed response body to the setter function.
+        setReviews(data);
+      })
+      .catch(console.error);
+    // An empty dependency array means the hook only runs when
+    // component launches.
+  }, []);
+
   return (
     <div className="App">
       <Header />
@@ -22,7 +41,12 @@ function App() {
       and element attributes as shown. */
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/reviews" element={<Reviews />} />
+        {/* Pass reviews as a prop to the Reviews component. */}
+        <Route path="/reviews" element={<Reviews reviews={reviews} />} />
+        <Route
+          path="/reviews/ :reviewId"
+          element={<Review reviews={reviews} />}
+        />
         <Route path="/about-me" element={<AboutMe />}>
           <Route path="contact" element={<Contact />} />
           <Route path="hobbies" element={<Hobbies />} />
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,7 +1,21 @@
-function Reviews() {
+import { Link } from "react-router-dom";
+import "./Reviews.css";
+import Review from "../Review/Review";
+
+function Reviews({ reviews }) {
   return (
-    <div>
+    <div className="reviews">
+      <ul className="reviews__list">
+        {reviews.map((review) => {
+          return (
+            <li key={review.id} className="reviews__item">
+              <Link to={`${review.id}`}>{review.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
       <p>My reviews will go here!</p>
+      <Review reviews={reviews} />
     </div>
   );
 }
